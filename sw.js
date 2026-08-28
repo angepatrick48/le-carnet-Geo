4bad1b33e0aeed7ffb170b5f4771e66af0f21ab6
angepatrick48 <kouameangepatrickmoro@gmail.com>
2026-08-28 17:28:19 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Le Carnet Géo — service worker : fonctionnement hors connexion */
-const CACHE = 'carnetgeo-v33';
+const CACHE = 'carnetgeo-v35';
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(['./','./index.html','./app.html'])).then(() => self.skipWaiting())
